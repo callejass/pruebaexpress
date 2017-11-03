067aa9a9791579d6dcbc5aa735e88ce6307a660c
Sergio <callejass@gmail.com>
2017-11-03 23:48:53 +0100

    Continuación

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-var productos=require("./routes/productos.js");
-var express=require("express");
-var app=express();
+
+var express     = require('express');
+
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+//var mongoose    = require('mongoose');
+
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
 
 
 
+//las rutas de nuestra api
+var productos=require("./routes/productos");
+var authentication=require("./routes/authentication");
+//var User   = require('./app/models/user'); // get our mongoose model
 
-/*app.get("/",function(req,res){
-    res.send("Hola mundo");
-});
-
-
-app.get("/prueba/",function(req,res){
-    res.send("Hola mundo prueba");
-});
-*/
-
-app.use("/api/v1/productos",productos);
+// =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 3000; // used to create, sign, and verify tokens
+//mongoose.connect(config.database); // connect to database
 
 
+var app = express();
+app.set('superSecret', config.secret); // secret variable
 
-app.listen("3000",function(){
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+
+//indicamos a la aplicacion las rutas que tiene que utilizar
+app.use("/api/authenticate",authentication);
+app.use("/api/productos",productos);
+
+
+
+app.listen(port,function(){
     console.log("Escuchando en el puerto 3000");
 
 });
